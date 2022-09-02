410cfd37d9fef7dbb9b5ca0bbcfb948859c15ceb
Ainul Md Razib <ainulmdrazib@Ainuls-MacBook-Pro.local>
2022-09-02 17:52:18 +0800

    Improve layout of todo list

--- src/components/TodoItems/TodoItem.js
+++ src/components/TodoItems/TodoItem.js
@@ -1,3 +1,5 @@
+import './TodoItem.css'
+
 function TodoItem(props){
   return (
     <div className="todo-item-box">
--- src/components/TodoItems/TodoItemList.js
+++ src/components/TodoItems/TodoItemList.js
@@ -1,9 +1,10 @@
 import TodoItem from "./TodoItem";
+import './TodoItemList.css'
 
 function TodoItemList(props){
     return (
       <div className="todo-list-box">
-        <h1>Tasks to do</h1>
+        <h1><em>Tasks to do</em></h1>
 
         <div className="todo-list">
             {props.todo_items_data.map((todo_item_data, i) => { 
