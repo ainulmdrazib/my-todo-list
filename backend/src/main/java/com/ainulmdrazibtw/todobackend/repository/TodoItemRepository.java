1a0c4dc4dbc6da6b48fc12625c05f086669af04a
Ainul Md Razib <ainulmdrazib@Ainuls-MacBook-Pro.local>
2022-09-30 16:43:47 +0800

    Include repository annotatn

--- backend/src/main/java/com/ainulmdrazibtw/todobackend/repository/TodoItemRepository.java
+++ backend/src/main/java/com/ainulmdrazibtw/todobackend/repository/TodoItemRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ainulmdrazibtw.todobackend.entity.TodoItemDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TodoItemRepository extends JpaRepository<TodoItemDetails, Integer> {
 
     List<TodoItemDetails> findAllByUsername(String username);
