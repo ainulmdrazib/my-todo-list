fde651f78f7e30ea8ad96b6fe945740cc27ebe14
Ainul Md Razib <ainulmdrazib@Ainuls-MacBook-Pro.local>
2022-10-05 13:42:08 +0800

    Implement and test form input and warning, improve todo item creation form design

---
+++ frontend/src/components/TodoForm/TodoForm.js
@@ -0,0 +1,40 @@
+import './TodoForm.css'
+
+import { useState } from "react";
+
+import TodoStrings from "../../constants/strings"
+
+function TodoForm(props){
+
+  const [todoTitle, setTodoTitle] = useState("");
+  const [titleError, setTitleError] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    
+    if (todoTitle !== ""){
+      setTitleError(false);
+      props.createTodo(todoTitle);
+    } else {
+      setTitleError(true);
+    }
+  };
+
+  return (
+    <div className="todo-form">
+        <form onSubmit={handleSubmit}>
+            <label className="title-label">
+              <strong>{TodoStrings.FORM_TITLE}</strong>
+            </label>
+            <input className="title-field" type="text" placeholder="Title of todo" value={todoTitle}
+            onChange={event => setTodoTitle(event.target.value)}/>
+
+            <button className="createButton" type="submit">{TodoStrings.FORM_BUTTON}</button>
+            <span className="title-error"><em>{titleError ? TodoStrings.ERROR_TITLE_IS_EMPTY : ""}</em></span>
+</form>
+    </div>
+  );
+}
+
+export default TodoForm;
---
+++ frontend/src/components/TodoForm/TodoForm.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import TodoForm from './TodoForm';
+import TodoStrings from "../../constants/strings"
+
+
+describe('TodoForm', () => {
+
+    const testPropErrors = {
+        "titleIsEmpty":""
+    }
+
+    const mockCreateTodo = jest.fn()
+
+    it('renders form components', () => {
+        const wrapper = mount(<TodoForm  errors={testPropErrors}/>);
+
+        expect(wrapper.find('.createButton').length).toBe(1);
+        expect(wrapper.find('.title-field').length).toBe(1);
+        expect(wrapper.find('.title-error').length).toBe(1);
+    })
+
+    it('submits title', () => {
+        const wrapper = mount(<TodoForm createTodo={mockCreateTodo} />);
+
+        const createButton = wrapper.find('.createButton');
+        const titleField = wrapper.find('.title-field');
+
+        titleField.simulate('change', { target: { value: 'todo item 1' } })
+
+        createButton.simulate("submit");
+
+        expect(mockCreateTodo).toBeCalledTimes(1);
+    });
+
+    it('rejects empty title and displays error message', () => {
+        const wrapper = mount(<TodoForm createTodo={mockCreateTodo}/>);
+
+        const createButton = wrapper.find('.createButton');
+        const titleError = wrapper.find('.title-error');
+
+        createButton.simulate("submit");
+
+        expect(titleError.text()).toBe(TodoStrings.ERROR_TITLE_IS_EMPTY);
+    });
+})
+
